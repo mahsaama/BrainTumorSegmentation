d1dcf7296c326cb4c18db977d63b71d495fc85d3
mahsa.ama <mahsa.ama1391@gmail.com>
2022-05-05 18:18:58 +0430

    dataset path fixed.

--- scripts/main.py
+++ scripts/main.py
@@ -56,11 +56,11 @@ class_weights = np.array([0.25659472, 45.465614, 16.543337, 49.11155], dtype="f"
 # show_folder_images("../Dataset_BRATS_2020/Validation/BraTS20_Validation_001/")
 
 # images lists
-t1_list = sorted(glob.glob("./Dataset_BRATS_2020/Training/*/*t1.nii.gz"))
-t2_list = sorted(glob.glob("./Dataset_BRATS_2020/Training/*/*t2.nii.gz"))
-t1ce_list = sorted(glob.glob("./Dataset_BRATS_2020/Training/*/*t1ce.nii.gz"))
-flair_list = sorted(glob.glob("./Dataset_BRATS_2020/Training/*/*flair.nii.gz"))
-seg_list = sorted(glob.glob("./Dataset_BRATS_2020/Training/*/*seg.nii.gz"))
+t1_list = sorted(glob.glob("../Dataset_BRATS_2020/Training/*/*t1.nii.gz"))
+t2_list = sorted(glob.glob("../Dataset_BRATS_2020/Training/*/*t2.nii.gz"))
+t1ce_list = sorted(glob.glob("../Dataset_BRATS_2020/Training/*/*t1ce.nii.gz"))
+flair_list = sorted(glob.glob("../Dataset_BRATS_2020/Training/*/*flair.nii.gz"))
+seg_list = sorted(glob.glob("../Dataset_BRATS_2020/Training/*/*seg.nii.gz"))
 
 # create the training and validation sets
 n_data = len(t1_list)
