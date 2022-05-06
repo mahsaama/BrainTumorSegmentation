c62a27eb4ea40651d34a47960b244454cd31a052
mahsa.ama <mahsa.ama1391@gmail.com>
2022-05-06 17:19:50 +0430

    add () to loss

--- utils/losses.py
+++ utils/losses.py
@@ -1,31 +1,5 @@
 import tensorflow as tf
 
-# class FocalLoss(nn.modules.loss._WeightedLoss):
-
-#     def __init__(self, gamma=2, size_average=None, ignore_index=-100,
-#                  reduce=None, balance_param=1.0):
-#         super(FocalLoss, self).__init__(size_average)
-#         self.gamma = gamma
-#         self.size_average = size_average
-#         self.ignore_index = ignore_index
-#         self.balance_param = balance_param
-
-#     def forward(self, input, target):
-
-#         # inputs and targets are assumed to be BatchxClasses
-#         assert len(input.shape) == len(target.shape)
-#         assert input.size(0) == target.size(0)
-#         assert input.size(1) == target.size(1)
-
-#         # compute the negative likelyhood
-#         logpt = - F.binary_cross_entropy_with_logits(input, target)
-#         pt = torch.exp(logpt)
-
-#         # compute the loss
-#         focal_loss = -( (1-pt)**self.gamma ) * logpt
-#         balanced_focal_loss = self.balance_param * focal_loss
-#         return balanced_focal_loss
-
 
 def diceLoss(y_true, y_pred, class_weights):
     y_true = tf.convert_to_tensor(y_true, "float32")
@@ -47,7 +21,7 @@ def diceLoss(y_true, y_pred, class_weights):
         + 1e-5
     )
 
-    return 1 - 2 * num / den
+    return 1 - (2 * num / den)
 
 
 def discriminator_loss(disc_real_output, disc_fake_output):
