7d530e058bc31b723e744df69ee3b780966bd0ce
mahsa.ama <mahsa.ama1391@gmail.com>
2022-05-22 14:29:45 +0430

    predict added to other models.

--- models/att_gan_model.py
+++ models/att_gan_model.py
@@ -321,3 +321,33 @@ class AttGAN:
             del Xb, yb, canvas, y_pred, y_true, idx
             print('Time: {}\n'.format(sec_to_minute(time.time()-start)))        
         return history
+    
+    def predict(self, test_gen):
+        start = time.time()
+        for i, Xb, yb in enumerate(test_gen):
+            dice_loss, dice_acc = self.test_step(Xb, yb)
+            # save pred image at epoch e
+            y_pred = self.G.predict(Xb)
+            y_true = np.argmax(yb, axis=-1)
+            y_pred = np.argmax(y_pred, axis=-1)
+
+            patch_size = test_gen.patch_size
+            canvas = np.zeros((patch_size, patch_size * 3))
+            idx = np.random.randint(len(Xb))
+
+            x = Xb[idx, :, :, patch_size // 2, 2]
+            canvas[0:patch_size, 0:patch_size] = (x - np.min(x)) / (
+                np.max(x) - np.min(x) + 1e-6
+            )
+            canvas[0:patch_size, patch_size : 2 * patch_size] = (
+                y_true[idx, :, :, patch_size // 2] / 3
+            )
+            canvas[0:patch_size, 2 * patch_size : 3 * patch_size] = (
+                y_pred[idx, :, :, patch_size // 2] / 3
+            )
+
+            fname = (self.path + "/test_{:03d}.png").format(i + 1)
+            mpim.imsave(fname, canvas, cmap="gray")
+
+            del Xb, yb, canvas, y_pred, y_true, idx
+        print("Time: {}\n Dice Accuracy: {}\n".format(sec_to_minute(time.time() - start), dice_acc))
