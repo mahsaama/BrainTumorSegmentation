bbda3b33b29ab01ceb18b4d6c04e3f81459ee568
mahsa.ama <mahsa.ama1391@gmail.com>
2022-05-30 20:06:25 +0430

    debug

--- utils/losses.py
+++ utils/losses.py
@@ -57,22 +57,22 @@ def generator_loss(target, gen_output, disc_fake_output, class_weights, alpha):
 def per_class_dice(y_true, y_pred, class_weights):
     y_true = tf.convert_to_tensor(y_true, "float32")
     y_pred = tf.convert_to_tensor(y_pred, y_true.dtype)
-
+    weights = class_weights.copy()
     # WT, TC, ET
     per_class_losses = []
     for i in range(3):
-        class_weights[i] = 0
+        weights[i] = 0
 
         num = tf.math.reduce_sum(
             tf.math.multiply(
-                class_weights,
+                weights,
                 tf.math.reduce_sum(tf.math.multiply(y_true, y_pred), axis=[0, 1, 2, 3]),
             )
         )
         den = (
             tf.math.reduce_sum(
                 tf.math.multiply(
-                    class_weights,
+                    weights,
                     tf.math.reduce_sum(tf.math.add(y_true, y_pred), axis=[0, 1, 2, 3]),
                 )
             )
